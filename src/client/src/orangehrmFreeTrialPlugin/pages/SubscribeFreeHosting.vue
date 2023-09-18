006379dd02c0214c08e5d5a6824676fbc5a88113
ManulMax <manul@orangehrm.us.com>
2023-09-18 10:46:46 +0530

    OHRM5X-2332: Add ondemand subscription form

---
+++ src/client/src/orangehrmFreeTrialPlugin/pages/SubscribeFreeHosting.vue
@@ -0,0 +1,274 @@
+<!--
+/*
+ * This file is part of OrangeHRM Inc
+ *
+ * Copyright (C) 2020 onwards OrangeHRM Inc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see  http://www.gnu.org/licenses
+ */
+-->
+
+<template>
+  <div v-if="isSubscribed">
+    <oxd-alert
+      :show="true"
+      type="error"
+      message="You have already subscribed for free-hosting"
+    ></oxd-alert>
+  </div>
+  <div v-if="!isSubscribed" class="orangehrm-background-container">
+    <div class="orangehrm-paper-container">
+      <div class="orangehrm-header-container">
+        <oxd-text tag="h6" class="orangehrm-main-title"
+          >Subscribe For Free Hosting</oxd-text
+        >
+      </div>
+      <div>
+        <oxd-divider />
+        <oxd-form
+          ref="subscribeForm"
+          :loading="isLoading"
+          @submit-valid="onSubmit"
+        >
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field label="System Url" :value="url" disabled />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field
+                  v-model="subscribe.companyName"
+                  :rules="rules.companyName"
+                  label="Company Name"
+                />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field
+                  v-model="subscribe.noOfEmployee"
+                  :rules="rules.noOfEmployee"
+                  label="No. of Employees"
+                />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field
+                  v-model="subscribe.country"
+                  type="select"
+                  :label="$t('general.country')"
+                  :options="countries"
+                  :rules="rules.country"
+                />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field
+                  v-model="subscribe.contactPersonName"
+                  :rules="rules.contactPersonName"
+                  label="Contact Person Name"
+                />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field
+                  v-model="subscribe.contactNumber"
+                  :rules="rules.contactNumber"
+                  label="Contact Number"
+                />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field
+                  v-model="subscribe.email"
+                  :rules="rules.email"
+                  label="Email"
+                />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <div class="orangehrm-action-button">
+            <oxd-form-actions>
+              <oxd-button
+                type="button"
+                display-type="ghost"
+                label="Cancel"
+                @click="onCancel"
+              />
+              <submit-button label="Submit" />
+            </oxd-form-actions>
+          </div>
+        </oxd-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {APIService} from '@ohrm/core/util/services/api.service';
+import {
+  shouldNotExceedCharLength,
+  validPhoneNumberFormat,
+  validEmailFormat,
+  required,
+  digitsOnly,
+  maxCurrency,
+} from '@ohrm/core/util/validation/rules';
+import {navigate, reloadPage} from '@/core/util/helper/navigation';
+import {OxdAlert} from '@ohrm/oxd';
+
+export default {
+  name: 'SubscribeFreeHosting',
+  components: {
+    'oxd-alert': OxdAlert,
+  },
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    contactPersonName: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: Object,
+      default: null,
+    },
+    isSubscribed: {
+      type: Boolean,
+      default: false,
+    },
+    countries: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  setup() {
+    const http = new APIService(
+      window.appGlobal.baseUrl,
+      `/api/v2/trial/subscribeFreeHosting`,
+    );
+    return {
+      http,
+    };
+  },
+
+  data() {
+    return {
+      isLoading: false,
+      subscribe: {
+        noOfEmployee: '',
+        companyName: this.companyName,
+        contactNumber: this.contactNumber,
+        email: this.email,
+        contactPersonName: this.contactPersonName,
+        country: this.country,
+        isSubscribed: this.isSubscribed,
+      },
+      rules: {
+        email: [required, shouldNotExceedCharLength(320), validEmailFormat],
+        contactPersonName: [required, shouldNotExceedCharLength(150)],
+        companyName: [required, shouldNotExceedCharLength(255)],
+        contactNumber: [
+          required,
+          shouldNotExceedCharLength(15),
+          validPhoneNumberFormat,
+        ],
+        noOfEmployee: [digitsOnly, maxCurrency(1000000000)],
+        country: [required],
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.subscribe.isSubscribed) {
+        navigate('/trial/subscribeFreeHosting');
+      }
+      this.isLoading = true;
+      this.http
+        .request({
+          method: 'POST',
+          data: {
+            companyName: this.subscribe.companyName,
+            contactNumber: this.subscribe.contactNumber,
+            email: this.subscribe.email,
+            contactPersonName: this.subscribe.contactPersonName,
+            noOfEmployee: this.subscribe.noOfEmployee,
+            country: this.subscribe.country.label,
+          },
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            this.subscribe.companyName = response.data.data.companyName;
+            this.subscribe.contactNumber = response.data.data.contactNumber;
+            this.subscribe.contactPersonName =
+              response.data.data.contactPersonName;
+            this.subscribe.country = response.data.data.country;
+            this.subscribe.email = response.data.data.email;
+            this.subscribe.noOfEmployee = response.data.data.noOfEmployees;
+            this.subscribe.isSubscribed = true;
+            this.$toast.success({
+              title: this.$t('general.success'),
+              message: 'Successfully Subscribed',
+            });
+          } else if (response.status === 400) {
+            navigate('/trial/subscribeFreeHosting');
+          }
+          return this.$toast.saveSuccess();
+        })
+        .then(() => {
+          this.isLoading = false;
+        });
+    },
+
+    onCancel() {
+      reloadPage();
+    },
+  },
+};
+</script>
+
+<style src="./subscribe-free-hosting.scss" lang="scss" scoped></style>
