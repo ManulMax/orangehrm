91ba15210974471b4408e693b9a36a3b5310967c
ManulMax <manul@orangehrm.us.com>
2023-12-06 16:18:21 +0530

    OHRM5X-2401: Fix for Cypress Test Failure

--- src/test/functional/cypress/integration/feature/admin/jobCategories.spec.js
+++ src/test/functional/cypress/integration/feature/admin/jobCategories.spec.js
@@ -64,6 +64,7 @@ describe('Admin - Job Category', function () {
       cy.getOXD('form').within(() => {
         cy.getOXDInput('Name')
           .type(this.strings.chars100.text)
+          .cy.wait(1000)
           .isInvalid('Should not exceed 50 characters');
         cy.getOXDInput('Name').setValue('').isInvalid('Required');
         cy.getOXDInput('Name')
